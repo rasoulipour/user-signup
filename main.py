6ecb6e42e2dd75d6fcf4a508dc057320aa0d36c8
Rasoulipour <rasoulipourmohammad@gmail.com>
2017-02-02 20:23:58 -0600

    work with an ancient system

--- main.py
+++ main.py
@@ -21,16 +21,16 @@ import re
 import cgi
 
 
-def page_builder(user_error, pass_error, verify_error, email_error):
+def page_builder(user_error, pass_error, verify_error, email_error, u, p, v, e):
     username_label = '<label>Username</label>'
     password_label = '<label>Password</label>'
     verpass_label  = '<label>Verify Password</label>'
     email_label    = '<label>Email (Optional)</label>'
 
-    username_input = '<input type = "text" name = "username">'
-    password_input = '<input type = "password" name = "password">'
-    verpass_input  = '<input type = "password" name = "verify">'
-    email_input    = '<input type = "text" name = "email">'
+    username_input = '<input type = "text" name = "username" value='+u+'>'
+    password_input = '<input type = "password" name = "password" value='+p+'>'
+    verpass_input  = '<input type = "password" name = "verify" value='+v+'>'
+    email_input    = '<input type = "text" name = "email" value='+e+'>'
 
     submit = '<input type="submit" />'
 
@@ -48,7 +48,7 @@ def page_builder(user_error, pass_error, verify_error, email_error):
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 Password_RE = re.compile(r"^.{3,20}$")
-Email_RE = re.compile(r"^[\S]+@[\S]+.[\S]$")
+Email_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
 
 def valid_username(username):
     return USER_RE.match(username)
@@ -56,16 +56,14 @@ def valid_username(username):
 def valid_pass(passw):
     return Password_RE.match(passw)
 
-def valid_email(email):
-    return Email_RE.match(email)
+def valid_email(ema):
+    return Email_RE.match(ema)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        content = page_builder("","","","")
+        username, password, verify, email = "" , "" , "" , ""
+        content = page_builder("","","","", username, password, verify, email)
         self.response.write(content)
-
-
-
 
 
     def post(self):
@@ -82,30 +80,31 @@ class MainHandler(webapp2.RequestHandler):
             content = "Welcome " + username
         else:
 
-            error_user = ""
-            error_pass = ""
+            error_user   = ""
+            error_pass   = ""
             error_verify = ""
-            error_email = ""
+            error_email  = ""
 
             if not valid_username(username):
                 error_user = "usename not valid."
+                username = ""
 
             if not valid_pass(password):
                 error_pass = "password not valid"
+                password = ""
 
             if password != verify:
                 error_verify = "passwords do not match"
+                verify = ""
 
-            if email != "" and not valid_email(email):
+            if not valid_email(email):
                 error_email = "email is not valid"
+                email = ""
 
-            content = page_builder(error_user,error_pass,error_verify,error_email)
+            content = page_builder(error_user,error_pass,error_verify,error_email,username, password, verify, email)
 
 
         self.response.write(content)
-
-
-
 
 
 
